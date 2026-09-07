780b4288ae96e912b8d58d06a94aa250892526f9
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-09-07 15:32:10 +0545

    feat: register attendance integrity API

--- app/src/hajiriflow/main.py
+++ app/src/hajiriflow/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from hajiriflow import __version__
+from hajiriflow.api.attendance import router as attendance_router
 from hajiriflow.api.biometric import router as biometric_router
 from hajiriflow.api.calendar_leave import router as calendar_leave_router
 from hajiriflow.api.csrf import router as csrf_router
@@ -50,6 +51,7 @@ def create_app() -> FastAPI:
     app.include_router(role_lifecycle_router)
     app.include_router(workforce_router)
     app.include_router(calendar_leave_router)
+    app.include_router(attendance_router)
     app.include_router(biometric_router)
     app.include_router(csrf_router)
     return app
