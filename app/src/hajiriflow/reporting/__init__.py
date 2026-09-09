f66b372d30e7b31acb21f970c6e9800a09e34637
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-09-09 13:19:24 +0545

    feat: add reporting service package

---
+++ app/src/hajiriflow/reporting/__init__.py
@@ -0,0 +1,5 @@
+"""Attendance reporting, exports, and employee self-service services."""
+
+from hajiriflow.reporting.service import AttendanceReportingService
+
+__all__ = ["AttendanceReportingService"]
